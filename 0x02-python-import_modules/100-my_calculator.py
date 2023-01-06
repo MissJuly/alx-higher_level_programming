fcb1a72fc855022c9c9160fcea439853143a6503
MissJuly <joygitau98@gmail.com>
2023-01-06 21:53:07 +0300

    Add my calculator file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,15 +6,10 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] == "+":
-        print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
-    elif argv[2] == "-":
-        print("{} - {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
-    elif argv[2] == '*':
-        print("{} * {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
-    elif argv[2] == "/":
-        print("{} / {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
-
-    else:
+    a, b = int(argv[1]), int(argv[3])
+    operator = argv[2]
+    math = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in math:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
