081ffadf1f3459fe86c631588a0e87f653fd0e86
Joy <joygitau98@gmail.com>
2023-03-15 23:02:23 +0300

    Add updates

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
---Creates database hbtn_0d_usa and table states.
+-- Creates database hbtn_0d_usa and table states.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
         PRIMARY KEY(`id`),
