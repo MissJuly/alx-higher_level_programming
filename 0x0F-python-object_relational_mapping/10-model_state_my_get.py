41501e4df25216d549d60cc70e1e12cd3dde7895
MissJuly <joygitau98@gmail.com>
2023-04-16 03:36:52 +0300

    Add 10-model_state_my_get file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-lists all State objects that contain the letter a from
-the database hbtn_0e_6_usa
+prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4]),)
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
     try:
         print(instance[0].id)
     except:
