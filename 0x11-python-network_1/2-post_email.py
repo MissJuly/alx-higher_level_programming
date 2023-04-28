bda40d02a1e4c3d6960c49ccf00fe49013cf804e
MissJuly <joygitau98@gmail.com>
2023-04-28 22:26:05 +0300

    Add task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.arv[1]
+    url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
